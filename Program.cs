77d74d7689fd98b1cf244c3d0e000f6a6816305d
Jacob Leatherwood <jacob.leatherwood.22@gmail.com>
2017-09-06 08:17:58 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace WorkingWithTextExercises
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
